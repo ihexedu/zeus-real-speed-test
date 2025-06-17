405836f82dc177d4fa5fb46127f5d7a7bf926dea
ihexedu <rick.twomey@zeusitandsecurity.com>
2025-06-17 08:05:01 -0400

    Delete upload.js

--- upload.js
+++
@@ -1,7 +0,0 @@
-
-export default async function handler(req, res) {
-  const start = Date.now();
-  await req.body;
-  const duration = (Date.now() - start) / 1000; // seconds
-  res.status(200).send(duration.toString());
-}
