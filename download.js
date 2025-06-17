aee88bc19706031eb7f8a3a1c3a14d389204dc52
ihexedu <rick.twomey@zeusitandsecurity.com>
2025-06-17 08:03:37 -0400

    Delete download.js

--- download.js
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  const size = 2 * 1024 * 1024; // 2 MB
-  const buffer = Buffer.alloc(size, '0');
-  res.setHeader('Content-Type', 'application/octet-stream');
-  res.setHeader('Content-Length', buffer.length);
-  res.send(buffer);
-}
